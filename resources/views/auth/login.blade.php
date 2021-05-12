57a025915582d872cf7f9e2ea44add7ac116bdec
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-12 11:43:30 +0800

    add some changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,23 @@
 @section('content')
     <main>
         <div class="login-container flex h-screen">
-            <div class="login xs:w-full xs:ml-10 xs:mr-10 sm:w-96 sm:m-auto rounded-lg m-auto p-4 bg-white">
-                <p class="text-center mb-4">Login</p>
-                <input type="text" name="username" class="input" placeholder="username">
+            <div class="bg-white m-auto login xs:max-w-xs xs:m-auto sm:w-96 sm:m-auto rounded-lg rounded-lg shadow-lg p-4 py-8">
+                <p class="font-bold text-center mb-6">Login</p>
+                <div class="flex justify-center mb-4">
+                    <input type="text" name="username"
+                        class="input" 
+                        placeholder="username" required>
+                </div>
+                <div class="flex justify-center">
+                    <input type="password" name="username" 
+                        class="input"
+                        placeholder="password" required>
+                </div>
+                <div class="flex justify-center">
+                    <button class="bg-blue-400 mt-6 p-4 rounded-lg text-center text-white w-3/4 ">
+                        login
+                    <button>
+                </div>
             </div>
         </div>
     </main>
