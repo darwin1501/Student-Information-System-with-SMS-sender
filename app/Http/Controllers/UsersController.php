a2b67067dd801664228fc4da55fd8402056893ba
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-14 14:51:20 +0800

    get all users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,14 +8,19 @@ use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-    //
-    public function createUser(){
-        $user = new User;
-
-        $user->username = 'user101';
-        $user->email = 'user@email.com';
-        $user->password = Hash::make('1234');
-        $user->user_type = 'user';
-        $user->save();
+    public function getUsers()
+    {
+        // redirect and set value for the header
+        return view('web.users.users')->with([
+            'header' => 'Users'
+        ]);
     }
+    // public function createUser(){
+    //     $user = new User;
+    //     $user->username = 'user101';
+    //     $user->email = 'user@email.com';
+    //     $user->password = Hash::make('1234');
+    //     $user->user_type = 'user';
+    //     $user->save();
+    // }
 }
