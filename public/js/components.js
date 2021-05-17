d1149d01e2eac1b52aff4b096df1366b38b52c5c
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-17 16:32:36 +0800

    added side nav close and open button

---
+++ public/js/components.js
@@ -0,0 +1,9 @@
+
+// side nav interaction
+const openNav = (()=>{
+    document.getElementById("mySidenav").style.width = "200px";
+})
+
+const closeNav = (()=>{
+    document.getElementById("mySidenav").style.width = "0";
+})
