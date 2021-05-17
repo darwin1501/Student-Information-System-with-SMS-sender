acae413e4d862de89a3fc85d56bfe6f7d33b2ce7
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-17 16:33:34 +0800

    added logic on sidenav

--- resources/views/include/sidenav.blade.php
+++ resources/views/include/sidenav.blade.php
@@ -1,10 +1,12 @@
 <div id="mySidenav" class="sidenav shadow-lg">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     @if (auth()->user()->user_type === 'admin')
-      <a href="#">Users</a>
-      <a href="#">Students</a>
+      <a href="{{ route('users') }}">Users</a>
+      <a href="{{ route('students') }}">Students</a>
     @else
-      <a href="#">Students</a>
+      <a href="{{ route('students') }}">Students</a>
     @endif
     <a href="#">Class</a>
   </div>
+  {{-- side nav menu icon --}}
+  <span class="sidenav-icon" onclick="openNav()">&#9776;</span>
