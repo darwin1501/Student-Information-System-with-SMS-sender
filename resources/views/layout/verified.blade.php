f7ab6e7cd8d84dcdfa83eae6422cd33a0e2efbac
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-17 11:18:47 +0800

    change file

--- resources/views/layout/verified.blade.php
+++ resources/views/layout/verified.blade.php
@@ -1,9 +1,30 @@
-@extends('layout.main')
+{{-- @section('content')
+    @include('include.header')
+    @include('include.sidenav') --}}
+    {{-- side nav menu icon --}}
+    {{-- <span class="sidenav-icon" onclick="openNav()">&#9776;</span>
+    @yield('main-content') --}}
+{{-- @endsection --}}
 
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Grade Sender</title>
+</head>
+<body class="bg-gray-200">
     @include('include.header')
     @include('include.sidenav')
     {{-- side nav menu icon --}}
     <span class="sidenav-icon" onclick="openNav()">&#9776;</span>
-    @yield('main-content')
-@endsection
+    @yield('content')
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/index.js') }}"></script>
+</html>
