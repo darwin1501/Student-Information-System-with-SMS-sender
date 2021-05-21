271d6cee28ea01304f20e9dafac03412ea9ab8a4
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-21 20:50:29 +0800

    added seeder generator

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('users')->delete();
+        
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'username' => 'admin',
+                'email' => 'admin@email.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$4z/k9pAMMB9.6vnnH0mMuOBJwqCftnK/WjOGahq7V07wtVDDyQRU6',
+                'user_type' => 'admin',
+                'status' => 1,
+                'remember_token' => NULL,
+                'created_at' => '2021-05-20 12:53:11',
+                'updated_at' => '2021-05-20 12:53:11',
+            ),
+        ));
+        
+        
+    }
+}
