adc9ce91ad9ce807fcf66c11bc80445cad58d79e
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-21 20:51:11 +0800

    added request validation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Database\QueryException;
 
 class UsersController extends Controller
 {
@@ -22,22 +25,63 @@ class UsersController extends Controller
         return $users;
     }
 
-    public function createUser(Request $request){
-        $username = $request->json('username');
-        // check username if already exists
-        $usernameDuplicate = User::select('username')
-                    ->where('username', $username);
-        // count existing username
-        return $usernameDuplicate->count();
+    // public function createUser(StoreUserRequest $request){
+    //     $username = $request->json('username');
+    //     $email = $request->json('email');
+    //     $password = $request->json('password');
+    //     // check username if already exists
+    //     // $usernameDuplicate = User::select('username')
+    //     //             ->where('username', $username);
+    //     // // count existing username
+    //     // return $usernameDuplicate->count();
 
-        // handle duplicate email error
+    //     // handle duplicate email error
+    //     try{
+    //         // store to database
+    //         // DB::table('users')->insert([
+    //         //     'name' => $request->json('name'),
+    //         //     'email' => $request->json('email'),
+    //         //     'created_at' =>  \Carbon\Carbon::now()
+    //         // ]);
+
+    //     // save to database
+    //         $user = new User;
+    //         $user->username = $username;
+    //         $user->email = $email;
+    //         $user->password = Hash::make($password);
+    //         $user->user_type = 'user';
+    //         $user->status = 1;
+    //         $user->save();
+
+    //         return "stored successfully";
+    //     }
+    //     catch(QueryException $e){
+    //         $errorCode = $e->errorInfo[1];
+    //         // check email already exists
+    //         // email has unique attributes
+    //         // if($errorCode == 1062)
+    //         // {
+    //         //     return "usedEmail";
+    //         // }
+    //         return $errorCode;
+    //     }
+    // }
+
+    //validates the request using StoreUserRequest 
+    public function createUser(StoreUserRequest $request){
+        $username = $request->json('username');
+        $email = $request->json('email');
+        $password = $request->json('password');
 
         // save to database
-        // $user = new User;
-        // $user->username = 'user105';
-        // $user->email = 'user105@email.com';
-        // $user->password = Hash::make('1234');
-        // $user->user_type = 'user';
-        // $user->save();
+        $user = new User;
+        $user->username = $username;
+        $user->email = $email;
+        $user->password = Hash::make($password);
+        $user->user_type = 'user';
+        $user->status = 1;
+        $user->save();
+
     }
+
 }
