aca1b5be1cfc297395800dfc6abcd590276d717d
darwin1501 <darwin.pablo1501@gmail.com>
2021-05-21 20:51:34 +0800

    added status field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('user_type');
+            $table->integer('status');
             $table->rememberToken();
             $table->timestamps();
         });
