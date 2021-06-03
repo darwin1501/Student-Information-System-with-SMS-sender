a2ebf212a62c286b3779449e14d947d21e04f5dd
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-03 14:20:38 +0800

    add some middleware

--- routes/web.php
+++ routes/web.php
@@ -27,23 +27,36 @@ Route::get('/', [LoginController::class, 'loginPage'])->name('loginPage');
 // Route::get('/send', [MessageController::class, 'sendSMS']);
 
 // Route::get('/create', [UsersController::class, 'createUser']);
-Route::get('/profile/{user}', [ProfileController::class, 'getUserProfile']);
-Route::post('/updateprofile/{user}', [ProfileController::class, 'updateUserProfile']);
-Route::post('/updatepassword/{user}', [ProfileController::class, 'updatePassword']);
 
+// profiles
+Route::get('/profile/{user}', [ProfileController::class, 'getUserProfile'])
+->middleware('auth');
+Route::post('/updateprofile/{user}', [ProfileController::class, 'updateUserProfile'])
+->middleware('auth');
+Route::post('/updatepassword/{user}', [ProfileController::class, 'updatePassword'])
+->middleware('auth');
+
+// admin only
 Route::get('/users', [UsersController::class, 'usersPage'])
 ->name('users')
 ->middleware(['auth', 'checkPermission']);
-Route::get('/block/{user}', [UsersController::class, 'blockUser']);
-Route::get('/unblock/{user}', [UsersController::class, 'unblockUser']);
-Route::delete('/deleteuser/{user}', [UsersController::class, 'deleteUser']);
-Route::get('/userlist', [UsersController::class, 'getAllUsers'])->middleware('checkStatus','checkPermission');
-Route::post('/createuser', [UsersController::class, 'createUser']);
-Route::get('/searchUser/{username}', [UsersController::class, 'searchUser']);
+Route::get('/block/{user}', [UsersController::class, 'blockUser'])
+->middleware(['auth', 'checkPermission']);;
+Route::get('/unblock/{user}', [UsersController::class, 'unblockUser'])
+->middleware(['auth', 'checkPermission']);
+Route::delete('/deleteuser/{user}', [UsersController::class, 'deleteUser'])
+->middleware(['auth', 'checkPermission']);
+Route::get('/userlist', [UsersController::class, 'getAllUsers'])
+->middleware(['auth', 'checkPermission']);
+Route::post('/createuser', [UsersController::class, 'createUser'])
+->middleware(['auth', 'checkPermission']);
+Route::get('/searchUser/{username}', [UsersController::class, 'searchUser'])
+->middleware(['auth', 'checkPermission']);
 
-Route::get('/students', [StudentsController::class, 'getStudents'])->middleware(['auth', 'checkStatus'])
+// contributor and admin
+Route::get('/students', [StudentsController::class, 'getStudents'])
+->middleware(['auth', 'checkStatus'])
 ->name('students');
 
 Route::post('/login', [LoginController::class, 'verifyUser'])->name('login');
-
 Route::get('/logout', [LogOutController::class, 'logOut'])->name('logout');
