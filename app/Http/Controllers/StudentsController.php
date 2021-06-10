b3c65ea0efb4c036c1f721ac6c25960ea318b1e9
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-10 18:56:30 +0800

    add functions that store students info

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -6,11 +6,25 @@ use Illuminate\Http\Request;
 
 class StudentsController extends Controller
 {
-    public function getStudents()
+    public function studentsPage()
     {
         // redirect and set value for the header
         return view('web.students.students')->with([
             'header' => 'Students'
         ]);
+    }
+
+    public function addStudent(Request $request)
+    {
+        $studentName = $request->json('studentName');
+        $phoneNumber = $request->json('phoneNumber');
+
+        $request->user()->students()->create([
+            'students_name' => $studentName,
+            'phone_number' => $phoneNumber,
+            'created_by' => auth()->user()->username
+        ]);
+
+        return 'success';
     }
 }
