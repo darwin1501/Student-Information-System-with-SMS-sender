a8803690151a5af34270455c4dfc063922365a8e
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-10 18:54:17 +0800

    added add student button

--- resources/views/web/students/students.blade.php
+++ resources/views/web/students/students.blade.php
@@ -1,6 +1,12 @@
 @extends('layout.verified')
 @section('content')
-    
+<div class="flex justify-center">
+    <button onclick="showAddStudentModal()"
+    class="ml-auto py-1 px-3 text-center text-xs text-white rounded-full bg-blue-400">
+        +&nbsp;Student
+    <button>
+</div>
+@include('include.modals.students.add_students')
 @endsection
 
 @section('jsLogic')
