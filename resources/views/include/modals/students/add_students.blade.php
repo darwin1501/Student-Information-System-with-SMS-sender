f6854a0ac60a34f0259a5574ae6b8baa2acc1980
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-11 17:12:29 +0800

    add some classname and properties

--- resources/views/include/modals/students/add_students.blade.php
+++ resources/views/include/modals/students/add_students.blade.php
@@ -9,12 +9,12 @@
             <div class="input-container">
                 <form onsubmit="return addStudent()">
                     <input type="text" name="students-name" id="students-name" 
-                        class="border-2 border-gray-300 user-inputs mb-2 p-2 w-full bg-gray-200 rounded-lg text-center text-sm" 
-                        placeholder="students-name" required>
+                        class="_clear-onsucess border-2 border-gray-300 user-inputs mb-2 p-2 w-full bg-gray-200 rounded-lg text-center text-sm" 
+                        placeholder="students name" required>
 
                     <input type="number" name="phone-number" id="phone-number" 
-                        class="border-2 addUserForm border-gray-300 user-inputs mb-2 p-2 w-full bg-gray-200 rounded-lg text-center text-sm"
-                        max="11"
+                        class="_clear-onsucess border-2 addUserForm border-gray-300 user-inputs mb-2 p-2 w-full bg-gray-200 rounded-lg text-center text-sm"
+                        maxlength="11"
                         placeholder="phone number" required>
 
                     <button class="w-full bg-blue-400 rounded-lg text-white text-center text-sm p-2 pr-2 pl-2">Add</button>
