cbd1a1182e3f27b0f023047d086bde7cac8c3d2c
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-11 17:11:35 +0800

    add table and pagination links

--- resources/views/web/students/students.blade.php
+++ resources/views/web/students/students.blade.php
@@ -1,10 +1,42 @@
 @extends('layout.verified')
 @section('content')
-<div class="flex justify-center">
-    <button onclick="showAddStudentModal()"
-    class="ml-auto py-1 px-3 text-center text-xs text-white rounded-full bg-blue-400">
-        +&nbsp;Student
-    <button>
+<div class="flex justify-center z-0">
+    {{-- table card --}}
+    <div class="card w-3/4 bg-white shadow-lg p-6 rounded-lg">
+        {{-- add user --}}
+        <div class="flex justify-center">
+            <button onclick="showAddStudentModal()"
+                class="ml-auto py-1 px-3 text-center text-xs text-white rounded-full bg-blue-400">
+                +&nbsp;Student
+            <button>
+        </div>
+        <div class="flex justify-center mt-5">
+            <table id="table" class=" font-sans table-auto p-2 text-xs w-full text-center">
+                <thead>
+                    <tr class="p-2 bg-gray-300">
+                        <th class="p-2 text-gray-700">Name</th>
+                        <th class="p-2 text-gray-700">Phone NUmber</th>
+                        <th class="p-2 text-gray-700">Added by</th>
+                        <th class="p-2 text-gray-700">Date Added</th>
+                        <th class="p-2 text-gray-700">Options</th>
+                    </tr>
+                </thead>
+                <tbody id="tbl-main-content">
+                </tbody>
+                </table>
+        </div>
+        <p id="no-result" class="hidden mt-4 font-bold text-center text-sm">No Results Found</p>
+        <p id="empty-students" class="hidden mt-4 font-bold text-center text-sm">No students added yet.</p>
+        {{-- 
+            copy the current page url on pagination
+            this will use to reload the current page when updating/changing data 
+            --}}
+        <input type="hidden" id="currentPageLink">
+        <div class="flex justify-start mt-5 text-sm">
+            <div class="pagination-links" id="paginationLinks">
+            </div>
+        </div>
+    </div>
 </div>
 @include('include.modals.students.add_students')
 @endsection
