1faf97966b85b243e7f68e353f3ffee921ae9a03
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-24 17:17:28 +0800

    fix spaces

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreUserRequest;
 
+
 class ProfileController extends Controller
 {
     public function getUserProfile(User $user)
