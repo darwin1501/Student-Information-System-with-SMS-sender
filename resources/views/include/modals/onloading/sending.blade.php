45e9acf479cde35023eedb395020e68451c16617
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-25 17:03:00 +0800

    add sending animation

---
+++ resources/views/include/modals/onloading/sending.blade.php
@@ -0,0 +1,8 @@
+<div id="sending" class="fixed flex h-screen hidden inset-0 items-center justify-center z-50">
+    {{-- card --}}
+        <div class="absolute bg-white m-auto p-6 r-0 rounded-lg w-1/4">
+            <div>
+                <p class="text-center">Sending. . .</p>
+            </div>
+        </div>
+    </div>
