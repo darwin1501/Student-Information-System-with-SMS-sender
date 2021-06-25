743f1d33aa94804ccb636adcdceefd4467550d01
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-25 17:06:08 +0800

    add dynamic sms config setting

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Faker\Provider\bn_BD\PhoneNumber;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 
 use SMSGatewayMe\Client\ApiClient;
+use Faker\Provider\bn_BD\PhoneNumber;
 use SMSGatewayMe\Client\Configuration;
 use SMSGatewayMe\Client\Api\MessageApi;
 use SMSGatewayMe\Client\Model\SendMessageRequest;
@@ -14,17 +15,16 @@ class MessageController extends Controller
 {
     public function sendGroupSms(Request $request)
     {
-      
+        // get sms config
+        $smsConfig = Setting::get()->first();
     
         // Configure client
         $config = Configuration::getDefaultConfiguration();
-        $config->setApiKey('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTYyMDI2NTc1NCwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjg4NTEyLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.J7v4-5-lEjhYurjfEchF7UfO106SJAC0L2n8Inub8-U');
+        $config->setApiKey('Authorization', $smsConfig->api_key);
         $apiClient = new ApiClient($config);
         $messageClient = new MessageApi($apiClient);
 
         // loop each request and send sms
-        // $students = json_decode($request);
-
         foreach($request->all() as $student) { 
             $phoneNumber = $student['phoneNumber'];
             $message = $student['message'];
@@ -33,22 +33,12 @@ class MessageController extends Controller
             $sendMessageRequest1 = new SendMessageRequest([
                 'phoneNumber' => $phoneNumber,
                 'message' => $message,
-                'deviceId' => 124289
+                'deviceId' => $smsConfig->device_id,
             ]);
 
             $sendMessages = $messageClient->sendMessages([
                 $sendMessageRequest1
             ]);
         }
-        // // Sending a SMS Message
-        // $sendMessageRequest1 = new SendMessageRequest([
-        //     'phoneNumber' => '',
-        //     'message' => '',
-        //     'deviceId' => 124289
-        // ]);
-
-        // $sendMessages = $messageClient->sendMessages([
-        //     $sendMessageRequest1
-        // ]);
     }
 }
