05afcab9aff05a88131a7ef2c67d4b466e843155
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-25 17:05:24 +0800

    add update setting

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -12,4 +12,11 @@ class SettingController extends Controller
         $smsConfig = Setting::get()->first();
         return $smsConfig;
     }
+
+    public function saveSetting(Setting $setting,Request $request)
+    {
+        $setting->api_key = $request->json('smsApiKey');
+        $setting->device_id = $request->json('deviceId');
+        $setting->save();
+    }
 }
