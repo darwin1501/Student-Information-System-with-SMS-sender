c798381ad26a0d8dc4da6eb0ae1ac438c6431ce0
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-25 17:02:47 +0800

    add update functionality at setting

--- resources/views/include/modals/setting/setting.blade.php
+++ resources/views/include/modals/setting/setting.blade.php
@@ -6,14 +6,22 @@
         <p class="text-center">Setting</p><br>
         <div class="flex justify-center mt-2 text-xs">
             <div>
-                <p>SMS Gateway Api Configuration</p><br><br>
+                <p>SMS Gateway Api Configuration</p><br>
+                <div class="border-b border-gray-300 w-full">
+                </div><br><br>
+                    <input type="hidden" id="setting-id">
                 <p class="mb-2 text-xs">API Key</p>
-                    <input type="text" name="smstoken" class="bg-gray-300 mb-2 p-2 rounded-lg text-center w-full" 
-                        id="sms-api-key"><br>
-                <p class="mb-2 text-xs">Device ID</p>
-                    <input type="text" name="smstoken" class="bg-gray-300 mb-2 p-2 rounded-lg text-center w-1/4" 
-                        id="device-id"><br><br>
-                <p class="mb-2 text-xs">Get API key and Device ID after you register here <a class="cursor-pointer">https://www.smsgateway.me</a></p>
+                    <form onsubmit="return saveSetting()">
+                        <input type="text" name="smstoken" class="bg-gray-300 mb-2 p-2 rounded-lg text-center w-full" 
+                            id="sms-api-key" required><br>
+                    <p class="mb-2 text-xs">Device ID</p>
+                        <input type="text" name="smstoken" class="bg-gray-300 mb-2 p-2 rounded-lg text-center w-1/4" 
+                            id="device-id" required><br><br>
+                    <p class="mb-2 text-xs">Get API key and Device ID after you register here <a class="cursor-pointer text-blue-400 text center">https://https://smsgateway.me/register</a></p>
+                    <div class="flex justify-center mb-2 mt-6">
+                        <button class="bg-blue-400 p-1 rounded-lg text-center text-white w-2/4">Save Changes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
