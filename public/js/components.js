22cdb865662df32f29565225fe6ff0b2325d3dee
darwin1501 <darwin.pablo1501@gmail.com>
2021-06-25 17:03:36 +0800

    add setting behavior and update

--- public/js/components.js
+++ public/js/components.js
@@ -9,12 +9,13 @@ const closeNav = (()=>{
     document.getElementById("mySidenav").style.width = "0";
 })
 
-// setting UI behavior
+// setting behavior
 const settingModal = document.getElementById('setting')
 
 const openSetting = () => {
     const smsApiKey = document.getElementById('sms-api-key');
     const deviceId = document.getElementById('device-id');
+    const settingId = document.getElementById('setting-id');
     // get sms config
     axios.get('/setting')
     .then((response) => {
@@ -23,6 +24,7 @@ const openSetting = () => {
         // display on element
         smsApiKey.value = response.data.api_key;
         deviceId.value = response.data.device_id;
+        settingId.value = response.data.id;
     })
     .catch((error) => {
         console.log(error);
@@ -33,3 +35,19 @@ const closeSetting = () => {
     settingModal.classList.add('hidden');
     settingModal.classList.remove('bg-gray-500', 'bg-opacity-70');
 }
+
+const saveSetting = () => {
+    const smsApiKey = document.getElementById('sms-api-key').value;
+    const deviceId = document.getElementById('device-id').value;
+    const settingId = document.getElementById('setting-id').value;
+
+    axios.post(`/savesetting/${settingId}`, {smsApiKey: smsApiKey, deviceId: deviceId})
+    .then((response) => {
+        alert('Setting Successfully Save');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+    return false;
+}   
